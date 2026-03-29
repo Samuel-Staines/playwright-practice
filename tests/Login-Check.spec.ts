88cabf4eb6c2d061a733fdf4bd4967e93015ed0b
Ultimetex <stuart0628@gmail.com>
2026-03-29 16:21:58 +0530

    Fixed webkit Error

--- tests/Login-Check.spec.ts
+++ tests/Login-Check.spec.ts
@@ -2,9 +2,11 @@ import { test, expect } from '@playwright/test';
 
 test('testing login page', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
+  await page.getByPlaceholder('Username').click();
   await page.getByPlaceholder('Username').fill('standard_user');
+  await page.getByPlaceholder('Username').click();
   await page.getByPlaceholder('Password').fill('secret_sauce');
   await page.getByRole('button', { name: 'Login'}).click();
 
-  expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+  await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
 });
